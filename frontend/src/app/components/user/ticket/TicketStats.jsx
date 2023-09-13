860b04ff91f557ff1419b5b88843766681b150e6
shauneang <shauneangjx@gmail.com>
2023-09-13 22:45:26 +0800

    Ticket Stats component spacing

--- frontend/src/app/components/user/ticket/TicketStats.jsx
+++ frontend/src/app/components/user/ticket/TicketStats.jsx
@@ -5,33 +5,36 @@ import React from 'react'
 export default function TicketStats() {
     return (
         <Card sx={{ padding: 1, width: "100%", alignItems: 'center', '& hr': { mx: 2, }, display: 'inline-flex', justifyContent: 'space-evenly' }} >
-            <Box overflow={'hidden'} textOverflow={'ellipsis'}>
-                <Box display={'inline-flex'}>
-                    <Typography variant='body2' color={"#9f9f9f"}>Min Price &#40;24H&#41;</Typography>
-                    <Tooltip title="Lowest 24 hour last sale price for NFTs in this token contract"><HelpOutline fontSize='small' sx={{ color: "#9f9f9f" }} /></Tooltip>
-                </Box>
-                <Typography>$160</Typography>
-            </Box>
-            <Divider orientation='vertical' variant='inset' flexItem />
-            <Box overflow={'hidden'} textOverflow={'ellipsis'}>
-                <Paper sx={{ display: "flex", flexDirection: 'column' }}>
-                    <Box display={'inline-flex'}>
-                        <Typography variant='body2' color={"#9f9f9f"} rel="preconnect">Last Sale &#40;Item&#41;</Typography>
-                        <Tooltip title="Lowest 24 hour last sale price for NFTs in this token contract"><HelpOutline fontSize='small' sx={{ color: "#9f9f9f" }} /></Tooltip>
-                    </Box>
-                    <Typography>$180</Typography>
-                </Paper>
-            </Box>
-            <Divider orientation='vertical' variant='inset' flexItem />
-            <Box overflow={'hidden'} textOverflow={'ellipsis'}>
-                <Paper sx={{ display: "flex", flexDirection: 'column' }}>
+            <Grid container display={'inline-flex'}>
+
+                <Grid item xs overflow={'hidden'} textOverflow={'ellipsis'}>
                     <Box display={'inline-flex'}>
                         <Typography variant='body2' color={"#9f9f9f"}>Min Price &#40;24H&#41;</Typography>
                         <Tooltip title="Lowest 24 hour last sale price for NFTs in this token contract"><HelpOutline fontSize='small' sx={{ color: "#9f9f9f" }} /></Tooltip>
                     </Box>
                     <Typography>$160</Typography>
-                </Paper>
-            </Box>
-        </Card>
+                </Grid>
+                <Divider orientation='vertical' variant='inset' flexItem />
+                <Grid item xs overflow={'hidden'} textOverflow={'ellipsis'}>
+                    <Box sx={{ width: "100%", display: "flex", flexDirection: 'column' }}>
+                        <Box display={'inline-flex'}>
+                            <Typography variant='body2' color={"#9f9f9f"} rel="preconnect">Last Sale &#40;Item&#41;</Typography>
+                            <Tooltip title="Last sale price for this Ticket"><HelpOutline fontSize='small' sx={{ color: "#9f9f9f" }} /></Tooltip>
+                        </Box>
+                        <Typography>$180</Typography>
+                    </Box>
+                </Grid>
+                <Divider orientation='vertical' variant='inset' flexItem />
+                <Grid item xs overflow={'hidden'} textOverflow={'ellipsis'}>
+                    <Box sx={{ display: "flex", flexDirection: 'column' }}>
+                        <Box display={'inline-flex'}>
+                            <Typography variant='body2' color={"#9f9f9f"}>Last Sale &#40;Contract&#41;</Typography>
+                            <Tooltip title="Last sale price for tickets in this token contract" ></Tooltip>
+                        </Box>
+                        <Typography>$160</Typography>
+                    </Box>
+                </Grid>
+            </Grid>
+        </Card >
     )
 }
