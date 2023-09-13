ceb56a3d9702765c84c8f97d7ba906b221331e6f
shauneang <shauneangjx@gmail.com>
2023-09-13 23:28:18 +0800

    ticket details

--- frontend/src/app/components/user/ticket/TicketStats.jsx
+++ frontend/src/app/components/user/ticket/TicketStats.jsx
@@ -9,8 +9,8 @@ export default function TicketStats() {
 
                 <Grid item xs overflow={'hidden'} textOverflow={'ellipsis'}>
                     <Box display={'inline-flex'}>
-                        <Typography variant='body2' color={"#9f9f9f"}>Min Price &#40;24H&#41;</Typography>
-                        <Tooltip title="Lowest 24 hour last sale price for NFTs in this token contract"><HelpOutline fontSize='small' sx={{ color: "#9f9f9f" }} /></Tooltip>
+                        <Typography variant='body2' color={"#5A5A5A"}>Min Price &#40;24H&#41;</Typography>
+                        <Tooltip title="Lowest 24 hour last sale price for NFTs in this token contract"><HelpOutline fontSize='small' sx={{ color: "#5A5A5A", marginLeft: 1 }} /></Tooltip>
                     </Box>
                     <Typography>$160</Typography>
                 </Grid>
@@ -18,8 +18,8 @@ export default function TicketStats() {
                 <Grid item xs overflow={'hidden'} textOverflow={'ellipsis'}>
                     <Box sx={{ width: "100%", display: "flex", flexDirection: 'column' }}>
                         <Box display={'inline-flex'}>
-                            <Typography variant='body2' color={"#9f9f9f"} rel="preconnect">Last Sale &#40;Item&#41;</Typography>
-                            <Tooltip title="Last sale price for this Ticket"><HelpOutline fontSize='small' sx={{ color: "#9f9f9f" }} /></Tooltip>
+                            <Typography variant='body2' color={"#5A5A5A"} rel="preconnect">Last Sale &#40;Item&#41;</Typography>
+                            <Tooltip title="Last sale price for this Ticket"><HelpOutline fontSize='small' sx={{ color: "#5A5A5A", marginLeft: 1 }} /></Tooltip>
                         </Box>
                         <Typography>$180</Typography>
                     </Box>
@@ -28,8 +28,8 @@ export default function TicketStats() {
                 <Grid item xs overflow={'hidden'} textOverflow={'ellipsis'}>
                     <Box sx={{ display: "flex", flexDirection: 'column' }}>
                         <Box display={'inline-flex'}>
-                            <Typography variant='body2' color={"#9f9f9f"}>Last Sale &#40;Contract&#41;</Typography>
-                            <Tooltip title="Last sale price for tickets in this token contract" ></Tooltip>
+                            <Typography variant='body2' color={"#5A5A5A"}>Last Sale &#40;Contract&#41;</Typography>
+                            <Tooltip title="Last sale price for tickets in this token contract" ><HelpOutline fontSize='small' sx={{ color: "#5A5A5A", marginLeft: 1 }} /></Tooltip>
                         </Box>
                         <Typography>$160</Typography>
                     </Box>
