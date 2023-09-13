2e6866762cb5ea69d5de76b71a718b310711211f
shauneang <shauneangjx@gmail.com>
2023-09-14 00:02:58 +0800

    Ticket art design

--- frontend/src/app/components/user/ticket/TicketArt.jsx
+++ frontend/src/app/components/user/ticket/TicketArt.jsx
@@ -3,22 +3,25 @@ import React from 'react'
 
 export default function TicketArt() {
     return (
-        <Card sx={{ borderRadius: "10px", padding: "10px", width: "100%" }} alignItems="center">
+        <Box
+
+            sx={{
+                objectFit: "contain",
+                aspectRatio: 1,
+                width: "100%",
+                borderRadius: "10px",
+                display: "grid",
+                alignItems: "center",
+                background: "linear-gradient(75deg,#f70a47,#f803bb,yellow,#f76e0a,#0af747,#0af7d3);",
+                boxShadow: 3,
+                padding: 3,
+                justifyContent: "center"
+            }}
+        >
             <Box
                 component="img"
                 src="https://www.snapdragonstadium.com/assets/img/Static_Digital-MainEventPage_1440x500_Coldplay_2023_Regional_SnapdragonStadium_0227_HER_070Shake_NEWSHOWADDED-f830d8441d.jpg"
-                sx={{
-                    objectFit: "contain",
-                    aspectRatio: 1,
-                    width: "100%",
-                    borderRadius: "10px",
-                    alignItems: "center",
-                    background: "linear-gradient(140deg, #EADEDB 0%, #BC70A4 50%, #BFD641 75%)",
-                    boxShadow: 3,
-                    padding: 3,
-                }}
-            >
-            </ Box>
-        </Card>
+            />
+        </ Box>
     )
 }
