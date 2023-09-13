d4c0400744b60907573cbe4f613810fdab26477e
shauneang <shauneangjx@gmail.com>
2023-09-14 00:24:59 +0800

    Ticket Activity Data Grid

--- frontend/src/app/components/user/ticket/TicketActivity.jsx
+++ frontend/src/app/components/user/ticket/TicketActivity.jsx
@@ -1,22 +1,13 @@
 import React from 'react'
-import { Card, Collapse, Divider, Typography } from '@mui/material'
-import List from '@mui/material/List';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import ExpandLess from '@mui/icons-material/ExpandLess';
-import ExpandMore from '@mui/icons-material/ExpandMore';
-import ListAltIcon from '@mui/icons-material/ListAlt';
-import TicketEventDetails from '@/app/components/user/ticket/TicketEventDetails'
-import TicketNftDetails from '@/app/components/user/ticket/TicketNftDetails'
-import { Event } from '@mui/icons-material';
+import { Card, Divider, Typography } from '@mui/material'
+import TicketActivityDataGrid from '@/app/components/user/ticket/TicketActivityDataGrid'
 
 export default function TicketActivity() {
     return (
         <Card >
             <Typography variant='h6' fontWeight={'bold'} sx={{ padding: 2 }}>Ticket Activity</Typography>
             <Divider />
-            <TicketEventDetails />
+            <TicketActivityDataGrid />
         </Card>
     )
 }
