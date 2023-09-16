342c030aa328bdfc971dac5337542913f6901496
JasonYapzx <jasonyapzx@gmail.com>
2023-09-16 10:49:56 +0800

    Fix component alias issues

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,12 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    resolve: {
+        alias: {
+          '@': path.resolve(__dirname, './src'),
+        },
+      },
+}
 
 module.exports = nextConfig
